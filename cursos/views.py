65d6d8a69059cf01dd3ee7ccfd41994e9954dca0
RafaelLeone <rinaciodacosta@gmail.com>
2022-07-07 10:29:14 -0300

    criado html e css para cursos e todos cursos

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def pagina_inicial(request):
+    return render(request, "cursos/pagina_inicial.html")
+
+def listar_cursos(request):
+    return render(request, "cursos/listar_cursos.html")
